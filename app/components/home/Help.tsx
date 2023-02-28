a7e800807c6c97130eb0f817e2e2926aa723a48c
unknown <Mohan>
2023-02-28 19:01:36 +0530

    help section

--- app/components/home/Help.tsx
+++ app/components/home/Help.tsx
@@ -31,27 +31,35 @@ const Help = () => {
       <div className="fixed-container">
         <div className="flex justify-between  gap-10">
           <div className="flex flex-1 flex-col justify-between">
-            <div>
-              <h2>
+            <div className="flex flex-col gap-4">
+              <h2 className="text-6xl font-normal text-white">
                 We're <br /> here to help
               </h2>
-              <p className="p2">YourGPT is very easy to setup and use</p>
+              <p className="text-white/60">
+                YourGPT is very easy to setup and use
+              </p>
               <div>
-                <img src="/images/home/power.svg" alt="" />
-                <button>Powered with ChatGPT</button>
+                <div className="help-btn">
+                  <img src="/images/home/power.svg" alt="" />
+                  <button className="btn">Powered with ChatGPT</button>
+                </div>
               </div>
             </div>
             <div>
-              <p className="p2">
+              <p className="text-white/70">
                 Looking for customised solution for your business ?
               </p>
-              <a>Reach out to us</a>
+              <a className="inline-block pt-4 text-white underline">
+                Reach out to us
+              </a>
             </div>
           </div>
           <div className="flex-[1.6]">
             <div>
-              <p className="p1">YourGPT</p>
-              <h3 className="">Use cases</h3>
+              <p className="p1 text-lg text-white/60">YourGPT</p>
+              <h4 className="mb-8 text-3xl font-semibold text-white">
+                Use cases
+              </h4>
             </div>
             <div className="grid grid-cols-2 gap-10">
               {CARD_DATA.map(({ src, label, text }) => {
@@ -74,6 +82,21 @@ const Help = () => {
 export default Help;
 
 const Root = styled.div`
+  .help-btn {
+    background-color: #faff0a;
+    display: inline-flex;
+    align-items: center;
+    gap: 5px;
+    padding: 0.5rem 1rem;
+    border-radius: 10px;
+    .btn {
+      outline: none;
+      border: none;
+      background-color: inherit;
+      color: #000;
+      font-weight: 600;
+    }
+  }
   .card {
     border-radius: 35px;
     cursor: pointer;
