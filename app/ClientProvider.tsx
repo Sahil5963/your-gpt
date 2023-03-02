48670fc8b99936489ecf285c25be1a93f9b6c006
Sahil <sk950121@gmail.com>
2023-03-02 18:59:26 +0530

    changes

--- app/ClientProvider.tsx
+++ app/ClientProvider.tsx
@@ -2,7 +2,7 @@
 import { CssVarsProvider } from '@mui/joy';
 import AuthProvider from 'context/AuthContext';
 import React from 'react';
-// import { Toaster } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
 
 export default function ClientProvider({
   children,
@@ -12,7 +12,7 @@ export default function ClientProvider({
   return (
     <AuthProvider>
       <CssVarsProvider defaultMode="light" modeStorageKey="joyTheme">
-        {/* <Toaster /> */}
+        <Toaster />
         {children}
       </CssVarsProvider>
     </AuthProvider>
