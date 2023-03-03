140d01cad7f079ab231943123c025f5f39a8a21d
unknown <Mohan>
2023-03-03 15:41:05 +0530

    sidebar design

--- app/(pages)/(external)/layout.tsx
+++ app/(pages)/(external)/layout.tsx
@@ -1,7 +1,9 @@
 /* eslint-disable @next/next/no-head-element */
-
+'use client';
+import styled from '@emotion/styled';
 import Navbar from 'app/components/Navbar';
 import { externalAppContent } from 'app/components/variants';
+import { EXTERNAL_THEME } from 'utils/ui';
 
 export default function RootLayout({
   children,
@@ -10,11 +12,26 @@ export default function RootLayout({
 }) {
   return (
     <>
-      <div className="">
-        <Navbar />
-
-        <main>{children}</main>
-      </div>
+      <Navbar />
+      <MainContent id="mainContent">{children}</MainContent>
+      <div className="bgDrop fixed top-0 left-0 right-0 z-20 h-screen w-full bg-black/50"></div>
     </>
   );
 }
+
+const MainContent = styled.main`
+  transition: all 0.4s;
+  &.main-offset {
+    transform: translateX(calc(100vw - ${EXTERNAL_THEME.navBarHeight}px));
+    transition: all 0.4s;
+
+    & + .bgDrop {
+      display: block;
+    }
+  }
+
+  & + .bgDrop {
+    display: none;
+    backdrop-filter: blur(3px);
+  }
+`;
